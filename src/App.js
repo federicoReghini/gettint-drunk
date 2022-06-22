9f23c756005bb44ad9d2792a861c34cf7c37d8cb
NicolaL95 <nicola.lilla@gmail.com>
2022-06-22 17:42:29 +0200

    reset app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { View } from "react-native";
-import { ButtonNf, HomeNf, JoinLobbyNf, LeaderBoard, Login, Signup, SwitchNf, LobbyContainer } from "./components";
+import { ButtonNf, HomeNf, JoinLobbyNf, LeaderBoard, Login, Signup, SwitchNf } from "./components";
 import CreateLobby from "./components/hooksComponents/pageComponents/CreateLobby";
-
 import { closeConnection, openConnection, sendDataToWs, wsMessage } from "./services/genericWebSocket";
 function App() {
   const [first, setfirst] = useState(false)
@@ -17,15 +16,12 @@ function App() {
     setfirst(!first)
   }
 
-
-
   return (
     <>
       <View style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         {/* <ButtonNf title='prova' onPress={()=> console.log('premuto')}/> */}
         {/* <SwitchNf isOn={first} onValueChange={()=> setfirst(!first)} /> */}
-
-        <LobbyContainer></LobbyContainer>
+        <JoinLobbyNf />
         {/* <LeaderBoard /> */}
         {/* <CreateLobby user={{ Id: 1, nickname: 'me' }} onTapStartGame={()=> console.log('tap')} /> */}
       </View>
