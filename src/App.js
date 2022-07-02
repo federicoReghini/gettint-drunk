359355338885e28100bfd99891340fc8fdb2e4f0
Federico Reghini <federicoreghini@gmail.com>
2022-07-02 12:56:32 +0200

    working on game

--- src/App.js
+++ src/App.js
@@ -1,73 +1,13 @@
 import React, { useEffect } from 'react';
-import { eventEmit, eventOn, getStorage, JoinLobbyNf } from './lib';
-import { createLobby } from './lib/services/api/lobbyapi';
-import { deleteApi } from './lib/services/genericServices';
-
-// library
-
-// navigation
-
-var WS = new WebSocket('ws://7emezzo-dev.eba-uwfpyt28.eu-south-1.elasticbeanstalk.com/ws')
-
-let id;
-let token;
-let lobby;
-let connectionEstablished;
+import { connectWS, eventEmit, eventOn, getStorage, JoinLobbyNf } from './lib';
+import { getMessage } from './lib/services/genericWebSocket';
 
 
-(async () => {
-  token = await getStorage('token')
-  await deleteApi('lobby', 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwb3RhdG8iLCJyb2xlcyI6WyJVU0VSIl0sImlhdCI6MTY1NjY4MzU1MSwiZXhwIjoxNjU2Njg3MTUxfQ.-n2v9ONknQ94i8I1AFuJVusMxg8y6SG2AN1GqtoISNw')
-  id = await getStorage('user');
 
-})()
-
-function App({navigation}) {
-
-
-  const sendMessage = (message) => {
-    WS.send(JSON.stringify(message));
-  }
-  useEffect(() => {
-    // WS.onopen = () => {
-    //   console.log("CONNECTED");
-    // }
-    WS.onmessage = (event) => {
-      console.log('onmessage', JSON.parse(event.data));
-      eventEmit('lobby', event.data)
-      lobby = JSON.parse(event.data)
-    }
-
-    createLobby('eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwb3RhdG8iLCJyb2xlcyI6WyJVU0VSIl0sImlhdCI6MTY1NjY4MzU1MSwiZXhwIjoxNjU2Njg3MTUxfQ.-n2v9ONknQ94i8I1AFuJVusMxg8y6SG2AN1GqtoISNw').then(response => {
-      // console.log(response.data);
-      lobby = response.data;
-
-      WS.onopen = () => {
-        console.log("CONNECTED");
-      }
-
-      connectionEstablished = false;
-      setTimeout(() => {
-        if (lobby != null && WS != null) {
-          const message = {
-            user_id: id,
-            method: "connectLobby"
-          }
-          sendMessage(message);
-          connectionEstablished = true;
-        }
-      }, 1000);
-    })
-
-  }, [WS.onmessage])
-
-  const handleNavigation = () => {
-    // navigation.navigate(routes.GAME);
-  }
-
+function App() {
 
   return (
-    <JoinLobbyNf onStartMatch={handleNavigation} id={id} />
+    <JoinLobbyNf />
   )
 }
 
